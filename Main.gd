2047ae7e61d0b46049b1f1c8fdb4e6abe4b19ebe
bendn <bend.n@outlook.com>
2022-11-24 10:10:13 +0700

    basic ui

--- Main.gd
+++ Main.gd
@@ -1,4 +1,1 @@
 extends Node2D
-
-func _init() -> void:
-    RenderingServer.set_default_clear_color(Color.BLACK)
--- autoloads/Globals.gd
+++ autoloads/Globals.gd
@@ -2,3 +2,6 @@ extends Node
 
 var player: Player = null
 var levelmanager: LevelManager = null
+
+func _init() -> void:
+    RenderingServer.set_default_clear_color(Color.BLACK)
