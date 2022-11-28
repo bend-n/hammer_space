d46a7365c2fd6c14c23b8c28fbb2eb3c8555ed8e
bendn <bend.n@outlook.com>
2022-11-28 14:35:04 +0700

    add music

--- Main.gd
+++ Main.gd
@@ -1,1 +1,7 @@
 extends Node2D
+
+func _ready() -> void:
+	MusicPlayer.p()
+
+func _exit_tree() -> void:
+	MusicPlayer.s()
---
+++ autoloads/MusicPlayer.gd
@@ -0,0 +1,18 @@
+extends AudioStreamPlayer
+
+
+var track := preload("res://assets/music/music.ogg")
+
+func _ready() -> void:
+	finished.connect(p)
+
+func p() -> void:
+	stream = track
+	pitch_scale = 1 + randf_range(-0.05, 0.05)
+	volume_db = -30
+	play()
+
+func s() -> void:
+	var tween := create_tween()
+	tween.tween_property(self, ^"volume_db", -200, 5)
+	tween.finished.connect(func stop() -> void: stop())
